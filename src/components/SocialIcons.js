830631cf29cc42c7322106f2153e01df51aea149
James Tricker <james@strangeworks.com>
2024-09-13 09:30:44 +0100

    update linkedin url

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -4,7 +4,7 @@ const SocialIcons = () => {
   return (
     <div className="social-icons">
       <a href="https://github.com/jamesquantum" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
-      <a href="https://www.linkedin.com/in/james-tricker/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+      <a href="https://www.linkedin.com/in/JamesQuantum/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
     </div>
   );
 };
